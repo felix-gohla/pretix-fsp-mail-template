134a1ca46012d75cf320c864537dc66879f650a3
Felix Gohla <felix.gohla@student.hpi.uni-potsdam.de>
2019-03-13 11:03:19 +0100

    Created MS Fröhlich Layout

--- pretix_fsp_mail/email.py
+++ pretix_fsp_mail/email.py
@@ -5,4 +5,4 @@ class FSPMailRenderer(TemplateBasedMailRenderer):
     verbose_name = 'FSP Mail Vorlage'
     identifier = 'fspmail'
     thumbnail_filename = 'pretix_fsp_mail/thumb.png'
-    template_name = 'pretix_fsp_mail/plainwrapper.html'
+    template_name = 'pretix_fsp_mail/fsp_plainwrapper.html'
